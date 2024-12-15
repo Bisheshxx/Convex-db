2f1218ffc368d380f8b756c065fe84774b5d20bc
“bishesh.tuladhar” <bishesh.tuladhar@ekbana.info>
2024-12-15 18:11:25 +1300

    fix: responsive design for different screen sizes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,7 +39,7 @@ export default function Home() {
         </div>
         {!isLoading ? (
           <div className="container mx-auto">
-            <div className="grid md:grid-cols-4 grid-cols-1 gap-4">
+            <div className="grid md:grid-cols-2 xl:grid-cols-4 grid-cols-1 gap-4">
               {tasks && tasks?.length > 0 ? (
                 tasks?.map((task: Tasks) => <Task key={task._id} task={task} />)
               ) : (
@@ -63,7 +63,7 @@ export default function Home() {
               {user?.unsafeMetadata?.type === "teacher" &&
                 tasks &&
                 tasks?.length > 0 && (
-                  <div className="md:mb-0 mb-0">
+                  <div className="md:mb-0 mb-2">
                     <CreateTaskDialog
                       DialogInitiator={
                         <Card className="h-full w-full flex justify-center items-center min-h-48">
